3af3f05d2a21588671c51495aa660cd101c0ba6c
Mark Cameron <mark.cameron@devfactory.ch>
2015-04-13 16:18:11 +0200

    PACKAGES: published Variables config

---
+++ laravel/config/variables.list.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Items added to the array should look like this:
+ *
+ *   'key' => array(
+ *     'value' => '',
+ *     'translation_key' => 'variables.key', // Create in your language files
+ *   ),
+ *
+ * Which might give something like:
+ *
+ *   'service_api_key' => array(
+ *     'value' => 'da46f8af58aec448c784dd421660f7635d404feb',
+ *     'translation_key' => 'variables.service_api_key',
+ *   ),
+ *
+ * @return
+ */
+
+return array(
+  'example' => array(
+    'value' => 'Example data!',
+    'translation_key' => 'variables.example',
+  ),
+);
---
+++ laravel/resources/views/vendor/variables/index.blade.php
@@ -0,0 +1,59 @@
+@extends($layout->extends)
+
+{{-- Page title --}}
+@section($layout->header)
+  <h1>@lang('variables::variables.page_title')</h1>
+@stop
+
+@section($layout->content)
+
+  <div class="row">
+
+    <div class="col-lg-8">
+
+      <div class="box box-primary">
+
+        {!! Form::open(array('url' => $edit_url, 'class' => 'form-horizontal')) !!}
+
+        <div class="box-body table-responsive no-padding">
+
+           <table class="table table-hover">
+
+            <thead>
+              <tr>
+                <th>@lang('variables::variables.table_key')</th>
+                <th>@lang('variables::variables.table_value')</th>
+              </tr>
+            </thead>
+
+            <tbody>
+
+              @foreach ($variables as $key => $variable)
+                <tr>
+                  <td class="col-sm-3 text-right">
+                    {!! Form::label($key, trans('variables.' . $key), array('class' => 'control-label')) !!}
+                  </td>
+                  <td>
+                    {!! Form::text($key, $variable['value'], array('class' => 'form-control')) !!}
+                  </td>
+                </tr>
+              @endforeach
+
+            </tbody>
+
+           </table>
+
+        </div>
+
+        <div class="box-footer clearfix">
+          {!! Form::submit(trans('variables::variables.save'), array('class' => 'btn btn-primary')) !!}
+        </div>
+
+        {!! Form::close() !!}
+
+      </div>
+
+    </div>
+  </div>
+
+@stop
