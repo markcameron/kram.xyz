b6ab7db8643d89d32b34762227111a1019d62474
Mark Cameron <mark.cameron@devfactory.ch>
2015-04-13 17:16:22 +0200

    TRANSLATIONS: Added menus.php

---
+++ laravel/resources/lang/en/menus.php
@@ -0,0 +1,16 @@
+<?php
+
+return array (
+  'admin' => 
+  array (
+    'dashboard' => 'Dashboard',
+    'content' => 'Content',
+    'users' => 'Users',
+    'users_manage' => 'Manage',
+    'users_groups' => 'Groups',
+    'configuration' => 'Configuration',
+    'config_translations' => 'Translations',
+    'config_variables' => 'Variables',
+    'logs' => 'logs',
+  ),
+);
