8165affbb8adc86855228ddcbdd19f38f79723c3
Mark Cameron <mark.cameron@devfactory.ch>
2015-04-27 13:43:42 +0200

    TRANSLATIONS: Updated and fixed missing translations

--- laravel/resources/lang/en/users.php
+++ laravel/resources/lang/en/users.php
@@ -1,8 +1,14 @@
 <?php
 
-# modified at 27-04-2015 11:41
+# modified at 27-04-2015 11:43
 
 return  array(
+	'create'                  =>  array(
+		'header'                  => 'Create User',
+	),
+	'edit'                    =>  array(
+		'header'                  => 'Edit User',
+	),
 	'email'                   => 'E-Mail',
 	'first_name'              => 'First Name',
 	'last_name'               => 'Last Name',
