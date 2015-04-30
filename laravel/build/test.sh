faf634cf7e98f754df27e6a7f739442f2f7f0675
Mark Cameron <mark.cameron@devfactory.ch>
2015-04-30 09:28:20 +0200

    BUILD: Added buidl folder from laravel-fork-me

---
+++ laravel/build/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# WORKING DIRECTORIES
+os=$(uname -a | awk '{print $1}')
+if [ $os == "Darwin" ] || [ $os == "MINGW32_NT-6.2" ];
+then
+    currpwd=$(pwd);
+    cwd=$(cd "$(dirname "$0")"; pwd);
+    cd $currpwd;
+else
+    cwd=$(dirname $(readlink -e $0));
+fi
+
+# Go to the main directory
+cd $cwd/..
+
+# Run the tests
+export LARAVEL_ENV=testing
+php artisan migrate:reset
+
+if [ "$cover" == "cover" ]
+then
+  php phpunit.phar --coverage-html build/coverage --log-junit build/junit.xml
+else
+  php phpunit.phar --log-junit build/junit.xml
+fi
