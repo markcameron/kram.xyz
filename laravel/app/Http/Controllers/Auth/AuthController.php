21eb282fb9e715ace623cbe2c63d9ac257303ca7
Mark Cameron <mark.cameron@devfactory.ch>
2015-06-10 09:55:37 +0200

    LARAVEL: Upgraded to 5.1

--- laravel/app/Http/Controllers/Auth/AuthController.php
+++ laravel/app/Http/Controllers/Auth/AuthController.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\Auth\Registrar;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Validator;
+use App\Models\User;
 
 class AuthController extends Controller {
 
@@ -29,13 +31,43 @@ class AuthController extends Controller {
 	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
 	 * @return void
 	 */
-	public function __construct(Guard $auth, Registrar $registrar)
+	public function __construct()
 	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
+//		$this->auth = $auth;
+//		$this->registrar = $registrar;
     $this->redirectPath = 'auth/login';
 
 		$this->middleware('guest', ['except' => 'getLogout']);
+	}
+
+
+	/**
+	 * Get a validator for an incoming registration request.
+	 *
+	 * @param  array  $data
+	 * @return \Illuminate\Contracts\Validation\Validator
+	 */
+	public function validator(array $data) {
+		return Validator::make($data, [
+			'name' => 'required|max:255',
+			'email' => 'required|email|max:255|unique:users',
+			'password' => 'required|confirmed|min:6',
+		]);
+	}
+
+	/**
+	 * Create a new user instance after a valid registration.
+	 *
+	 * @param  array  $data
+	 * @return User
+	 */
+	public function create(array $data) {
+		return User::create([
+			'first_name' => $data['first_name'],
+			'last_name' => $data['last_name'],
+			'email' => $data['email'],
+			'password' => bcrypt($data['password']),
+		]);
 	}
 
 }
